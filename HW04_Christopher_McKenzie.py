e2a7556e9c2305165d8b204ed9cba5f8dbdb4f55
Christopher McKenzie <camckenzie94@gmail.com>
2020-09-30 13:51:35 -0400

    Added error conditions, tests, and adjusted travis ci to test the correct file

--- HW04_Christopher_McKenzie.py
+++ HW04_Christopher_McKenzie.py
@@ -7,33 +7,64 @@ repository.
 import requests
 import json
 
-def getRepos(inp = "camckenzie"):
+def getRepos(inp: str = "Empty input"):
 
-    """Prompts input for GitHub user ID and provides number of repos
+    """Given a GitHub user ID, provides number of repos
     created by user and the corresponding number of commits for each
     commit.
     """
-    
+    if inp == "Empty input":
+        return "Error: Must input valid GitHub User ID."
+    elif type(inp) != str:
+        return "Error: Input must be a string."
+
     # Uncomment bellow to allow inputs from user.
     # Leave default input of camckenzie for Travis CI
     # inp = input('Please eneter GitHub user ID: ')
-    response_repos = requests.get("https://api.github.com/users/" + inp + "/repos")
-    # Creates dictionary out of JSON objects
-    dict_repos = response_repos.json()
+    
+    reponse = requests.get("https://api.github.com/users/" + inp + "/repos")
 
-    for repo in dict_repos:
-        
+    # Creates dictionary out of JSON objects
+    # Type changes to list if API rate limit exceeded or
+    # User has not created any repos
+    repos = reponse.json()
+
+    #return type(repos)
+    if type(repos) == list:
+        if "API rate limit exceeded" in repos:
+            # Unable to test for this unless rate limit reached
+            return "Error: API rate limit exceeded for machine. Please try again later."
+        elif repos == []:
+            return "Error: The user has not created any repos."
+    
+    if type(repos) == dict:
+        if "API rate limit exceeded" in repos['message']:
+            # Unable to test for this unless rate limit reached
+            return "Error: API rate limit exceeded for machine. Please try again later."
+    # This message exists when user inputs incorrect User ID
+        if repos['message'] == 'Not Found':
+            return "Error: The user does not exist."
+
+    # Easier to test for values within this list rather than standalone
+    # print statements.
+    repoData = []
+    for repo in repos:
+
         # Uses input for user and iterates through repo names from dictionary to create urls
         response_commits = requests.get("https://api.github.com/repos/" + inp + "/" + repo["name"] + "/commits")
         dict_commits = response_commits.json()
         key = '"commit:" {'
+
         # Below tracks number of times key variable appears in dictionary
         # This indicates the number of commits
         # counter = Counter(key for item in dict_commits)
         counter = 0
         for key in dict_commits:
             counter += 1
-        print("Repo: " + repo["name"] + "; Number of commits: " + str(counter))
+        
+        repoData.append("Repo: " + repo["name"] + "; Number of commits: " + str(counter))
+
+    return repoData
 
 
-
+print(getRepos("camckenzie"))
---
+++ HW04_Tests_Christopher_McKenzie.py
@@ -0,0 +1,41 @@
+"""Christopher McKenzie
+The purpose of this program is to test the getRepos function.
+"""
+
+import unittest
+import requests
+import json
+from HW04_Christopher_McKenzie import getRepos
+
+class TestGetRepos(unittest.TestCase):
+
+    def testGetReposChris(self):
+
+        """Tests valid user ID and its results"""
+
+        camckenzieData = ['Repo: CodeNames; Number of commits: 11',
+        'Repo: GitHubAPI; Number of commits: 17',
+        'Repo: hello-world; Number of commits: 3',
+        'Repo: Student-Repository; Number of commits: 22',
+        'Repo: Triangle; Number of commits: 15']
+
+        self.assertEqual(getRepos("camckenzie"), camckenzieData)
+
+    """The below tests error conditions and messages"""
+
+    def testNoInput(self):
+        self.assertEqual(getRepos(), "Error: Must input valid GitHub User ID.")
+
+    def testNoReposAccount(self):
+        self.assertEqual(getRepos("norepoaccount"), "Error: The user has not created any repos.")
+
+    def testNonStringInputs(self):
+        self.assertEqual(getRepos(1), "Error: Input must be a string.")
+        self.assertEqual(getRepos(1.1), "Error: Input must be a string.")
+    
+    def testNonExistentUser(self):
+        self.assertEqual(getRepos("NonExistentUser"), "Error: The user does not exist.")
+
+if __name__ == '__main__':
+    print('Running unit tests')
+    unittest.main()
